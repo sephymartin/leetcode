af9f8de6bdfe2ca8dc2f585c2603e2c64b160d39
sephy <sephymartin@gmail.com>
2025-02-22 21:05:04 +0800

    chore: p1

---
+++ src/main/java/top/sephy/leetcode/solution1/Solution.java
@@ -0,0 +1,30 @@
+package top.sephy.leetcode.solution1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i< nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i< nums.length; i++) {
+            int a = nums[i];
+            int b = target - a;
+            Integer index = map.get(b);
+            if (index != null && index != i) {
+                return new int[]{i, index};
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,4};
+        System.out.println(twoSum(nums, 6));
+    }
+}
